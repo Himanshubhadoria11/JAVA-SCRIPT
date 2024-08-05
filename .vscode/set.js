aee91d197617be64b70b0bf67411fe50d2c7fe42
Himanshubhadoria11 <himanshubhadoria22@gmail.com>
2024-08-05 11:37:40 +0530

    19/06/2024

--- .vscode/set.js
+++ .vscode/set.js
@@ -10,7 +10,7 @@
 
 //document.getElementById("a").innerHTML = "<center><h2>PNINFOSY Gwalior 1234</h2></center>";
 //document.getElementById("a").setAttribute("style","border:10px dotted red");
-document.getElementById("a").removeAttribute("style");
+//document.getElementById("a").removeAttribute("style");
 //document.getElementById("b").innerHTML = "<h1>GWALIOR</h1>";
 
 
